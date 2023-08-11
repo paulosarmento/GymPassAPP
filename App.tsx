b58dd478fae78e32d18371e2cab140e6c15ab538
Mac <mac@MacBook-Pro-de-Mac.local>
2023-08-11 09:29:19 -0300

    Personalizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, StatusBar } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
@@ -10,7 +9,19 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#202024",
+      }}
+    >
+      <StatusBar
+        backgroundColor="transparent"
+        translucent
+        barStyle="light-content"
+      />
       {fontsLoaded ? <Text>Hello World!</Text> : <View />}
     </View>
   );
